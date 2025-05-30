6ca7b49963155ad2425479eb8fab76bfd05ce18a
l00187667 <L00187667@atu.ie>
2025-05-30 16:49:05 +0000

    created the mlops code

---
+++ app/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, render_template
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+model = pickle.load(open("model/model.pkl", "rb"))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    volume = float(request.form['volume'])
+    prediction = model.predict(np.array([[volume]]))
+    return render_template('index.html', prediction_text=f'Predicted CO2 Emission: {prediction[0]:.2f} g/km')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
